89b2be9d53610b3eb442f065adfe8c077854c85c
Ralf Schoch <rschoch@econ-tec.de>
2020-12-04 11:13:29 +0100

    Green: Magic Number

--- CodingDojo/Kata/Potter.cs
+++ CodingDojo/Kata/Potter.cs
@@ -5,11 +5,14 @@ namespace Kata
 {
     public class Potter
     {
+        private decimal _singleBookPrice = 8m;
+
         public decimal Calculate(List<int> books)
         {
             if (books.Any())
             {
-                return 8m;
+                
+                return _singleBookPrice;
             }
 
             return 0m;
