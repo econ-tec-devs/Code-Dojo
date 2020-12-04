868faaf25f9eab634883fc449c8aa57b0adac5bc
Ralf Schoch <rschoch@econ-tec.de>
2020-12-04 11:15:15 +0100

    Green: refactor Tests

--- CodingDojo/KataTest/PotterTests.cs
+++ CodingDojo/KataTest/PotterTests.cs
@@ -6,31 +6,35 @@ namespace KataTest
 {
     public class PotterTests
     {
+        private Potter _target;
+
         [SetUp]
         public void Setup()
         {
+            _target = new Potter();
         }
 
         [Test]
         public void Calculate_EmptyListWithCountOfSameBooks_Return0()
         {
-            var target = new Potter();
             var countOfSameBooks = new List<int>();
             var expected = 0m;
 
-            decimal actual = target.Calculate(countOfSameBooks);
-
-            Assert.That(actual, Is.EqualTo(expected));
+            ActAndAssert(countOfSameBooks, expected);
         }
 
         [Test]
         public void Calculate_ListWithOneBook_Return8()
         {
-            var target = new Potter();
             var countOfSameBooks = new List<int>() { 1 };
             var expected = 8m;
 
-            decimal actual = target.Calculate(countOfSameBooks);
+            ActAndAssert(countOfSameBooks, expected);
+        }
+
+        private void ActAndAssert(List<int> countOfSameBooks, decimal expected)
+        {
+            decimal actual = _target.Calculate(countOfSameBooks);
 
             Assert.That(actual, Is.EqualTo(expected));
         }
