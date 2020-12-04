63e1cbbc7f6b68b9a9dbc7b151d8d34b69e17ddc
Ralf Schoch <rschoch@econ-tec.de>
2020-12-04 11:41:05 +0100

    Green: Add discount Array

--- CodingDojo/Kata/Potter.cs
+++ CodingDojo/Kata/Potter.cs
@@ -6,21 +6,20 @@ namespace Kata
     public class Potter
     {
         private decimal _singleBookPrice = 8m;
-        private decimal _discountTwoBooks = 0.95m;
+
+        private decimal[] _discount = {0m, 1m, 0.95m, 0.9m, 0.8m};
+
+        private decimal _discountTwoBooks   = 0.95m;
         private decimal _discountThreeBooks = 0.9m;
+        private decimal _discountFourBooks  = 0.8m;
 
         public decimal Calculate(List<int> books)
         {
             var filteredBooks = books.Where(x => x > 0).ToList();
 
-            if (filteredBooks.Count == 2)
+            if (filteredBooks.Count > 1)
             {
-                return _singleBookPrice * filteredBooks.Count * _discountTwoBooks;
-            }
-            
-            if (filteredBooks.Count == 3)
-            {
-                return _singleBookPrice * filteredBooks.Count * _discountThreeBooks;
+                return _singleBookPrice * filteredBooks.Count * _discount[filteredBooks.Count];
             }
             
             if (filteredBooks.Any())
