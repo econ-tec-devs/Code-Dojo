51a4a9641e195db22f14f500c5f549bb6935fa7f
Matthias Veenhuis <mveenhuis@econ-tec.de>
2020-12-10 11:10:56 +0100

    red test

--- CodingDojo/KataTest/SushiTests.cs
+++ CodingDojo/KataTest/SushiTests.cs
@@ -1,17 +1,32 @@
-using NUnit.Framework;
-
+// -----------------------------------------------------------------------
+// <copyright file="SushiTests.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
 namespace KataTest
 {
+    using System.Collections.Generic;
+    using NUnit.Framework;
+
     public class SushiTests
     {
         [Test]
-        public void GetPrice_InputPlates_OutputPrice()
+        public void GetPrice_InputPlates_ReturnPrice()
         {
-            var price = 0;
             var expected = 0;
-            var actual = 0;
 
-            actual = Sushi.GetPrice();
+            var actual = Sushi.GetPrice();
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void GetPrice_InputOneBluePlate_ReturnOnePrice()
+        {
+            var plates = new List<string> { };
+            var expected = 0;
+
+            var actual = Sushi.GetPrice(plates);
 
             Assert.That(actual, Is.EqualTo(expected));
         }
