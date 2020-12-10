cc464ef42795531a5dd6a0987f699b00292e5aae
Matthias Veenhuis <mveenhuis@econ-tec.de>
2020-12-10 11:05:10 +0100

    green test

---
+++ CodingDojo/KataTest/Sushi.cs
@@ -0,0 +1,15 @@
+// -----------------------------------------------------------------------
+// <copyright file="Sushi.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+namespace KataTest
+{
+    public class Sushi
+    {
+        public static int GetPrice()
+        {
+            return 0;
+        }
+    }
+}
