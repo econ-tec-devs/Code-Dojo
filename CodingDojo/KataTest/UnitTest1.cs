9ac33618925c5a8a2a9ee1ae77c29509538933bd
Dominik Hirsch <dhirsch@econ-tec.de>
2020-12-10 10:57:12 +0100

    First Red Test

--- CodingDojo/KataTest/UnitTest1.cs
+++ CodingDojo/KataTest/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace KataTest
@@ -10,9 +11,16 @@ namespace KataTest
         }
 
         [Test]
-        public void Test1()
+        public void CalculatePrice_NoDish_ReturnFullPrice()
         {
-            Assert.Pass();
+            var dishes = new Dictionary<string, int>();
+
+            var expected = 0;
+
+            var actual = Sushi.CalculatePrice(dishes);
+
+
+            Assert.That(actual,Is.EqualTo(expected));
         }
     }
 }
