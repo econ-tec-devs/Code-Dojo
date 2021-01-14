732e889989e10e6c45cbae2a0419780b222b6db5
Corinne Tzebia <ctzebia@econ-tec.de>
2021-01-14 11:36:46 +0100

    Green Test for one blue Dish

--- CodingDojo/Kata/Sushi.cs
+++ CodingDojo/Kata/Sushi.cs
@@ -6,6 +6,11 @@ namespace KataTest
     {
         public static decimal CalculateFullPrice(Dictionary<string, int> dishes)
         {
+            if (dishes.Count==1)
+            {
+                return dishes.Count * 0.95m;
+            }
+            
             return 0m;
         }
     }
