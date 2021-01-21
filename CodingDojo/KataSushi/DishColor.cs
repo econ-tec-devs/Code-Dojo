d39074ae581d60adc5d93ac4181d1239bee3059e
Rouven Lutz <rlutz@econ-tec.de>
2021-01-21 11:14:01 +0100

    Green test: OneRed

--- CodingDojo/KataSushi/DishColor.cs
+++ CodingDojo/KataSushi/DishColor.cs
@@ -2,6 +2,7 @@ namespace KataTest
 {
     public enum DishColor
     {
+        None,
         Blue,
         Red,
         Yellow,
--- CodingDojo/KataSushi/Sushi.cs
+++ CodingDojo/KataSushi/Sushi.cs
@@ -7,11 +7,21 @@ namespace KataTest
     {
         public static decimal Calculate(List<DishColor> basket)
         {
-            if (basket.Any())
+            if (!basket.Any())
+            {
+                return 0m;
+            }
+
+            if (basket[0] == DishColor.Blue)
             {
                 return 0.95m;
             }
-            
+
+            if (basket[0] == DishColor.Red)
+            {
+                return 1.95m;
+            }
+
             return 0m;
         }
     }
