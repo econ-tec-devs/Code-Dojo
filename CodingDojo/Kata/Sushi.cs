9e7dd494c04978ee5812899b7787ac2ae25090a7
Ralf Schoch <rschoch@econ-tec.de>
2021-01-21 11:12:51 +0100

    refactor

--- CodingDojo/Kata/Sushi.cs
+++ CodingDojo/Kata/Sushi.cs
@@ -5,24 +5,19 @@ namespace Kata
 {
     public class Sushi
     {
-        private static decimal priceBlueDish = 0.95m;
-        private static decimal priceRedDish = 1.95m;
-        private static decimal priceYellowDish = 2.95m;
+        private static Dictionary<DishColor, decimal> pricelist = new Dictionary<DishColor, decimal>()
+        {
+            {DishColor.Blue, 0.95m},
+            {DishColor.Red, 1.95m},
+            {DishColor.Yellow, 2.95m},
+        };
+
 
         public static decimal Calculate(List<DishColor> basket)
         {
-
             if (basket.Any())
             {
-                switch (basket.First())
-                {
-                    case DishColor.Red:
-                        return priceRedDish;
-                    case DishColor.Blue:
-                        return priceBlueDish;
-                    case DishColor.Yellow:
-                        return priceYellowDish;
-                }
+                return pricelist[basket.First()];
             }
 
             return 0m;
