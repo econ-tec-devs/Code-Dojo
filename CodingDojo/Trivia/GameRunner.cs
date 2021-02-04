ecda82f1a70bc26cdd8fb5b872417e4cc572d3e7
Dominik Hirsch <dhirsch@econ-tec.de>
2021-02-04 12:48:35 +0100

    added second main method for testing purpose only

--- CodingDojo/Trivia/GameRunner.cs
+++ CodingDojo/Trivia/GameRunner.cs
@@ -6,31 +6,33 @@ namespace Trivia
     {
         private static bool _notAWinner;
 
-        //public static void Main(string[] args)
-        //{
-        //    var aGame = new Game();
-
-        //    aGame.Add("Chet");
-        //    aGame.Add("Pat");
-        //    aGame.Add("Sue");
-
-        //    var rand = new Random();
-
-        //    do
-        //    {
-        //        aGame.Roll(rand.Next(5) + 1);
-
-        //        if (rand.Next(9) == 7)
-        //        {
-        //            _notAWinner = aGame.WrongAnswer();
-        //        }
-        //        else
-        //        {
-        //            _notAWinner = aGame.WasCorrectlyAnswered();
-        //        }
-        //    } while (_notAWinner);
-        //}
         public static void Main(string[] args)
+        {
+            var aGame = new Game();
+
+            aGame.Add("Chet");
+            aGame.Add("Pat");
+            aGame.Add("Sue");
+
+            var rand = new Random();
+
+            do
+            {
+                aGame.Roll(rand.Next(5) + 1);
+
+                if (rand.Next(9) == 7)
+                {
+                    _notAWinner = aGame.WrongAnswer();
+                }
+                else
+                {
+                    _notAWinner = aGame.WasCorrectlyAnswered();
+                }
+            } while (_notAWinner);
+        }
+
+        [Obsolete("Only for tests")]
+        public static void Main2(string[] args)
         {
             var aGame = new Game();
 
