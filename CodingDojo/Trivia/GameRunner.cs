237200b6c1d96a307a3e725a52d5502ba739ad22
Dominik Hirsch <dhirsch@econ-tec.de>
2021-02-04 11:38:16 +0100

    New Main-Method with random seed 1135 Old Main method stays as comment

--- CodingDojo/Trivia/GameRunner.cs
+++ CodingDojo/Trivia/GameRunner.cs
@@ -6,6 +6,30 @@ namespace Trivia
     {
         private static bool _notAWinner;
 
+        //public static void Main(string[] args)
+        //{
+        //    var aGame = new Game();
+
+        //    aGame.Add("Chet");
+        //    aGame.Add("Pat");
+        //    aGame.Add("Sue");
+
+        //    var rand = new Random();
+
+        //    do
+        //    {
+        //        aGame.Roll(rand.Next(5) + 1);
+
+        //        if (rand.Next(9) == 7)
+        //        {
+        //            _notAWinner = aGame.WrongAnswer();
+        //        }
+        //        else
+        //        {
+        //            _notAWinner = aGame.WasCorrectlyAnswered();
+        //        }
+        //    } while (_notAWinner);
+        //}
         public static void Main(string[] args)
         {
             var aGame = new Game();
@@ -14,7 +38,7 @@ namespace Trivia
             aGame.Add("Pat");
             aGame.Add("Sue");
 
-            var rand = new Random();
+            var rand = new Random(1135);
 
             do
             {
