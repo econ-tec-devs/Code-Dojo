7e8d6d53d95084982760fc55dba753293ec36b13
Dominik Hirsch <dhirsch@econ-tec.de>
2021-02-04 12:49:16 +0100

    Added GoldenMaster file and reference to production code

--- CodingDojo/KataTest/TriviaTests.cs
+++ CodingDojo/KataTest/TriviaTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace KataTest
 {
@@ -7,6 +10,25 @@ namespace KataTest
         [SetUp]
         public void Setup()
         {
+        }
+
+        [Test]
+        public void GoldenMaster()
+        {
+            var oldOut = Console.Out;
+
+            var path = @"..\GoldenMaster.txt";
+            var fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+
+            var sw = new StreamWriter(fs);
+            Console.SetOut(sw);
+            Console.WriteLine("Test");
+            //Trivia.GameRunner.Main2(new string[0]);
+            Console.SetOut(oldOut);
+            sw.Close();
+            fs.Close();
+
+            Assert.That(true, Is.True);
         }
 
         [Test]
