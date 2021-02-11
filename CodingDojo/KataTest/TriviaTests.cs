bb3e64099bab1303bd7f79a136955ee15ebc7f8d
Corinne Tzebia <ctzebia@econ-tec.de>
2021-02-11 11:49:13 +0100

    CreateFileWithCurrentGame and equal to GoldenMaster

--- CodingDojo/KataTest/TriviaTests.cs
+++ CodingDojo/KataTest/TriviaTests.cs
@@ -8,30 +8,44 @@ namespace KataTest
 {
     public class TriviaTests
     {
+        private string path;
+        
         [SetUp]
         public void Setup()
         {
+            path = @"..\..\..\GoldenMaster.txt";
         }
 
+        //[Test]
+        //public void GoldenMaster()
+        //{
+        //    var oldOut = Console.Out;
+        //    using (var sw = new StreamWriter(new FileStream(path, FileMode.Create)))
+        //    {
+        //        Console.SetOut(sw);
+        //        GameRunner.Main2(new string[0]);
+        //        Console.SetOut(oldOut);
+        //    }
+
+        //    Assert.That(true, Is.True);
+        //}
+
         [Test]
-        public void GoldenMaster()
+        public void GammeRunner_CreateFileWithCurrentGame_EqualToGoldenMaster()
         {
             var oldOut = Console.Out;
-            var path = @"..\..\..\GoldenMaster.txt";
-            using (var sw = new StreamWriter(new FileStream(path, FileMode.Create)))
+            var currentFile = @"..\..\..\CurrentOutput.txt";
+            using (var sw = new StreamWriter(new FileStream(currentFile, FileMode.Create)))
             {
                 Console.SetOut(sw);
                 GameRunner.Main2(new string[0]);
                 Console.SetOut(oldOut);
             }
 
-            Assert.That(true, Is.True);
-        }
+            var actual = File.ReadAllText(currentFile);
+            var expected = File.ReadAllText(path);
 
-        [Test]
-        public void MethodeToTest_Scenario_Expected()
-        {
-            Assert.That(true, Is.False);
+            Assert.That(actual, Is.EqualTo(expected));
         }
     }
 }
