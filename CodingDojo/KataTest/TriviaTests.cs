ef1a8f94e364ab95eb1a7f3db530a59a9cf110cf
Matthias Veenhuis <mveenhuis@econ-tec.de>
2021-02-11 11:22:25 +0100

    green test compare

--- CodingDojo/KataTest/TriviaTests.cs
+++ CodingDojo/KataTest/TriviaTests.cs
@@ -7,6 +7,7 @@ namespace KataTest
 {
     using System;
     using System.IO;
+    using System.Text;
     using NUnit.Framework;
     using Trivia;
 
@@ -20,7 +21,7 @@ namespace KataTest
             path = "C:\\Users\\Public\\Documents\\GoldenMaster.txt";
         }
 
-        [Test]
+        //[Test]
         public void MethodeToTest_CreateFileFromOut_ReturnIfFileExist()
         {
             var expected = true;
@@ -30,7 +31,31 @@ namespace KataTest
                 Console.SetOut(sw);
                 GameRunner.Main(1337);
             }
+
             var actual = File.Exists(path);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void MethodeToTest_ReadFile_ReturnNotEmptyString()
+        {
+            var expected = string.Empty;
+
+            var actual = File.ReadAllText(this.path);
+
+            Assert.That(actual, Is.Not.EqualTo(expected));
+        }
+
+        [Test]
+        public void MethodeToTest_CompareFileWithGameOutput_ReturnTrueIfEqual()
+        {
+            var expected = File.ReadAllText(this.path);
+
+            var stringBuilder = new StringBuilder();
+            Console.SetOut(new StringWriter(stringBuilder));
+            GameRunner.Main(1337);
+            var actual = stringBuilder.ToString();
 
             Assert.That(actual, Is.EqualTo(expected));
         }
