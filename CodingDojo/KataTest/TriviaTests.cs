33d84eaa83c9dc2748efca29e05c30dee1c993f6
Matthias Veenhuis <mveenhuis@econ-tec.de>
2021-02-11 10:49:24 +0100

    green test

--- CodingDojo/KataTest/TriviaTests.cs
+++ CodingDojo/KataTest/TriviaTests.cs
@@ -1,26 +1,38 @@
-﻿using System;
-using System.IO;
-using NUnit.Framework;
-using Trivia;
-
+﻿// -----------------------------------------------------------------------
+// <copyright file="TriviaTests.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
 namespace KataTest
 {
+    using System;
+    using System.IO;
+    using NUnit.Framework;
+    using Trivia;
+
     public class TriviaTests
     {
+        private string path;
+
         [SetUp]
         public void Setup()
         {
+            path = "C:\\Users\\Public\\Documents\\GoldenMaster.txt";
         }
 
         [Test]
-        public void MethodeToTest_Scenario_Expected()
+        public void MethodeToTest_CreateFileFromOut_ReturnIfFileExist()
         {
-            using (var sw = new StreamWriter(new FileStream("C:\\Users\\Public\\Documents\\GoldenMaster.txt", FileMode.Create)))
+            var expected = true;
+
+            using (var sw = new StreamWriter(new FileStream(this.path, FileMode.Create)))
             {
                 Console.SetOut(sw);
                 GameRunner.Main(1337);
-                Assert.That(true, Is.False);
             }
+            var actual = File.Exists(path);
+
+            Assert.That(actual, Is.EqualTo(expected));
         }
     }
 }
