0e53574276d7eb3a91f13b0a04d15cc57a9ac214
Ralf Schoch <rschoch@econ-tec.de>
2021-02-19 10:24:32 +0100

    Reset Kata to BankOcr

---
+++ CodingDojo/KataTest/BankOcrTests.cs
@@ -0,0 +1,17 @@
+// -----------------------------------------------------------------------
+// <copyright file="BankOcrTests.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+namespace KataTest
+{
+    using NUnit.Framework;
+
+    public class BankOcrTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
