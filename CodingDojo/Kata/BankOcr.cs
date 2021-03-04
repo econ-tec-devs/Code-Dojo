4e475e0eae7c9c36e57450d55fa965b4a27794d6
Ralf Schoch <rschoch@econ-tec.de>
2021-03-04 11:21:38 +0100

    Make Test Green New Test for Inject Reader

---
+++ CodingDojo/Kata/BankOcr.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace Kata
+{
+    public class BankOcr
+    {
+        public List<AccountNumber> ScanFile(string fileName)
+        {
+            return new List<AccountNumber>();
+        }
+    }
+}
--- CodingDojo/KataTest/BankOcrTests.cs
+++ CodingDojo/KataTest/BankOcrTests.cs
@@ -3,21 +3,37 @@
 //     Copyright (c) econ-tec GmbH. All rights reserved.
 // </copyright>
 // -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using Kata;
+using NSubstitute;
+
 namespace KataTest
 {
     using NUnit.Framework;
 
     public class BankOcrTests
     {
+        private BankOcr _target;
+
         [SetUp]
         public void Setup()
         {
+            _target = new BankOcr();
         }
 
         [Test]
         public void Ctor_None_NewInstanceNoException()
         {
-            var target = new BankOcr();
+            Assert.That(_target, Is.TypeOf<BankOcr>());
+        }
+
+        [Test]
+        public void Ctor_InjectReader_NoError()
+        {
+            IReader reader = Substitute.For<IReader>();
+
+            var target = new BankOcr(reader);
 
             Assert.That(target, Is.TypeOf<BankOcr>());
         }
@@ -25,17 +41,12 @@ namespace KataTest
         [Test]
         public void ScanFile_InputFileName_ReturnAccountNumbers()
         {
-            var target = new BankOcr();
             var fileName = "emptyfile.txt";
             var expected = 0;
 
-            List<AccountNumber> actual = target.ScanFile(fileName);
+            List<AccountNumber> actual = _target.ScanFile(fileName);
 
             Assert.That(actual.Count, Is.EqualTo(expected));
         }
-    }
-
-    public class BankOcr
-    {
     }
 }
