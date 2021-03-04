28c4f5f756ae23b44c10ec0e66248623a9c98dad
Christian Lichter <clichter@econ-tec.de>
2021-03-04 11:32:47 +0100

    green test

--- CodingDojo/KataBankOcr/BankOcr.cs
+++ CodingDojo/KataBankOcr/BankOcr.cs
@@ -1,9 +1,19 @@
 using System.Collections.Generic;
+using KataBankOcr.Interfaces;
 
 namespace KataBankOcr
 {
     public class BankOcr
     {
+        public BankOcr()
+        {
+            
+        }
+
+        public BankOcr(IReader reader)
+        {
+        }
+
         public List<AccountNumber> Scan(string filename)
         {
             return new List<AccountNumber>();
