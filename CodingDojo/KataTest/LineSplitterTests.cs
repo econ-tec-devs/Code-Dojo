57cd4dfb8e7c2942a1e814895bc3c59d20ca0eb0
Dominik Hirsch <dhirsch@econ-tec.de>
2021-03-18 11:45:28 +0100

    first red test for LineSplitter

---
+++ CodingDojo/KataTest/LineSplitterTests.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="LineSplitterTests.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+namespace KataTest
+{
+    using Kata;
+    using NUnit.Framework;
+
+    internal class LineSplitterTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Ctor_NewInstance_ImplementsILineSplitter()
+        {
+            var target = new LineSplitter() as ILineSplitter;
+            Assert.That(target, Is.Not.Null);
+        }
+    }
+}
