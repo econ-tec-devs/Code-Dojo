51e5177c5af09aee480d081ca2ad5b693e04ac1a
Rouven Lutz <rlutz@econ-tec.de>
2021-03-18 10:57:26 +0100

    Red Test: call reader with null argument

---
+++ CodingDojo/KataTest/ReaderTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace KataTest
+{
+    using System;
+
+    [TestFixture]
+    public class ReaderTests
+    {
+        private Reader _target;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _target = new Reader();
+        }
+
+        [Test]
+        public void Read_NullArgument_ThrowsArgumentNullException()
+        {
+            string filename = null;
+
+            Assert.Throws<ArgumentNullException>(() => _target.Read(filename));
+        }
+    }
+}
