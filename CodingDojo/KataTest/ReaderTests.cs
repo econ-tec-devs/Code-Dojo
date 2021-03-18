778723a85cd579cc2970cc03b7e06d51fdff69c3
Matthias Veenhuis <mveenhuis@econ-tec.de>
2021-03-18 11:12:57 +0100

    commit red test

--- CodingDojo/KataTest/ReaderTests.cs
+++ CodingDojo/KataTest/ReaderTests.cs
@@ -11,11 +11,23 @@ namespace KataTest
     public class ReaderTests
     {
         [Test]
-        public void Ctor_None_NewInstanceNoException()
+        public void Ctor_NewInstance_NoException()
         {
             IReader target = new Reader();
 
             Assert.That(target, Is.TypeOf<Reader>());
+        }
+
+        [Test]
+        public void Read_EmptyFile_ReturnEmptyLines()
+        {
+            IReader target = new Reader();
+            var fileName = "emptyFile.txt";
+            var expected = 0;
+
+            var actual = target.Read(fileName);
+
+            Assert.That(actual.Count, Is.EqualTo(expected));
         }
     }
 }
