dd16567e3d1a51c7c6357e0167804b0e43f533fe
Dominik Hirsch <dhirsch@econ-tec.de>
2021-03-18 11:00:49 +0100

    first red test for ReaderTests

---
+++ CodingDojo/KataTest/ReaderTests.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ReaderTests.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+namespace KataTest
+{
+    using Kata;
+    using NUnit.Framework;
+
+    public class ReaderTests
+    {
+        [Test]
+        public void Ctor_None_NewInstanceNoException()
+        {
+            IReader target = new Reader();
+
+            Assert.That(target, Is.TypeOf<IReader>());
+        }
+    }
+}
