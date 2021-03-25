65f7dcd33622bbbeaf78a2aa073c3bc8e13365d3
Christian Lichter <clichter@econ-tec.de>
2021-03-25 10:40:22 +0100

    green test list of entry

---
+++ CodingDojo/KataBankOcr/LineSplitter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using KataBankOcr.Interfaces;
+
+namespace KataBankOcr
+{
+    public class LineSplitter : ILineSplitter
+    {
+        public List<Entry> Split(List<Line> lines)
+        {
+            return new List<Entry>(){new Entry()};
+        }
+    }
+}
