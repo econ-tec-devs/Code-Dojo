b69c3acea5bb0831dd0a7ce4ecc5e74f286ff1f4
Matthias Veenhuis <mveenhuis@econ-tec.de>
2021-03-25 11:25:02 +0100

    commit red test

--- CodingDojo/KataTest/LineSplitterTests.cs
+++ CodingDojo/KataTest/LineSplitterTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using KataBankOcr;
 using KataBankOcr.Interfaces;
@@ -65,6 +66,29 @@ namespace KataTest
 
             Assert.That(actual, Is.TypeOf<List<Entry>>());
             Assert.That(actual.Count, Is.EqualTo(expected));
+        } 
+        
+    [Test]
+    public void Split_ValidListOfFourLinesWithContent_ReturnsListWithContentEntry()
+    {
+        var lines = new List<Line>()
+        {
+            new Line(){Text = "Test1"},
+            new Line(){Text = "Test2"},
+            new Line(){Text = "Test3"},
+            new Line(){Text = "Test4"}
+        };
+        var expected1 = "Test1";
+        var expected2 = "Test2";
+        var expected3 = "Test3";
+        var expected4 = "Test3";
+
+        var actual = _target.Split(lines);
+
+        Assert.That(actual.First().Lines[0].Text,Is.EqualTo(expected1));
+        Assert.That(actual.First().Lines[1].Text, Is.EqualTo(expected2));
+        Assert.That(actual.First().Lines[2].Text, Is.EqualTo(expected3));
+        Assert.That(actual.First().Lines[3].Text, Is.EqualTo(expected4));
         }
     }
 }
