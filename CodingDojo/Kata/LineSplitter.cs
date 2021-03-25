eb99fd153eeca97d1bfe8e5a3450fbb3e51f14a9
Matthias Veenhuis <mveenhuis@econ-tec.de>
2021-03-25 10:22:39 +0100

    commit green test

---
+++ CodingDojo/Kata/LineSplitter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Kata
+{
+    public class LineSplitter : ILineSplitter
+    {
+        public List<Entry> Split(List<string> lines)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
