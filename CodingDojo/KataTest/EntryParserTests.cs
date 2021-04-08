d4ae5dd0b943acb2a07b10ce8e3e303bd1edc13a
Corinne Tzebia <ctzebia@econ-tec.de>
2021-04-08 11:39:33 +0200

    Test for ListOfEntriesWith2Entry made red

--- CodingDojo/KataTest/EntryParserTests.cs
+++ CodingDojo/KataTest/EntryParserTests.cs
@@ -19,7 +19,7 @@ namespace KataTest
             _target = new EntryParser(_digitParser);
             Assert.That(_target, Is.Not.Null);
         }
-        
+
         [Test]
         public void Parse_ListOfEntries_EmptyListOfAccountNumbers()
         {
@@ -43,6 +43,21 @@ namespace KataTest
 
             Assert.That(actual, Is.Not.Empty);
             Assert.That(actual.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Parse_ListOfEntriesWith2Entry_ListWithTwoAccountNumber()
+        {
+            var entry = new Entry();
+            var entries = new List<Entry>
+            {
+                entry, entry
+            };
+
+            var actual = _target.Parse(entries);
+
+            Assert.That(actual, Is.Not.Empty);
+            Assert.That(actual.Count, Is.EqualTo(2));
         }
     }
 }
