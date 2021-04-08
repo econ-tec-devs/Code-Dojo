0a8c574fc8ae36b73299754c9b5c8e4ec795c963
Corinne Tzebia <ctzebia@econ-tec.de>
2021-04-08 11:04:02 +0200

    Test for Injecting Digit Parser in the Entryparser Constructor

---
+++ CodingDojo/KataBankOcr/Interfaces/IDigitParser.cs
@@ -0,0 +1,6 @@
+﻿namespace KataTest
+{
+    public interface IDigitParser
+    {
+    }
+}
---
+++ CodingDojo/KataTest/EntryParserTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KataBankOcr.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace KataTest
+{
+    [TestFixture]
+    class EntryParserTests
+    {
+        [Test]
+        public void Ctor_InjectDigitParser_ReturnsNoError()
+        {
+            var digitParser = Substitute.For<IDigitParser>();
+            IEntryParser target = new EntryParser(digitParser);
+
+            Assert.That(target, Is.Not.Null);
+        }
+    }
+}
