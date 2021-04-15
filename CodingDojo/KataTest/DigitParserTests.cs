a53a2fb7c0ce70625ae9e1e735f69da9704f4f96
Matthias Veenhuis <mveenhuis@econ-tec.de>
2021-04-15 11:36:35 +0200

    red test

---
+++ CodingDojo/KataTest/DigitParserTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KataBankOcr;
+using NUnit.Framework;
+
+namespace KataTest
+{
+    [TestFixture]
+    public class DigitParserTests
+    {
+        private DigitParser _target;
+
+        [SetUp]
+        public void Setup()
+        {
+            _target = new DigitParser();
+            Assert.That(_target, Is.Not.Null);
+        }
+
+        [Test]
+        public void Parse_OneEntry_EmptyListOfRawDigits()
+        {
+            var entry = new Entry()
+            {
+                Lines = new List<Line>()
+                {
+                    new Line(string.Empty),
+                    new Line(string.Empty),
+                    new Line(string.Empty),
+                    new Line(string.Empty)
+                }
+            };
+            
+            var actual = _target.Parse(entry);
+
+            Assert.That(actual, Is.Empty);
+        }
+    }
+}
