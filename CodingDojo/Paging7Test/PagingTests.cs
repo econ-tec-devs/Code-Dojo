a60d24dd0e6f0c7002fc839a74074b4a566d705c
Rouven Lutz <rlutz@econ-tec.de>
2021-05-06 11:08:44 +0200

    First red test

--- CodingDojo/Paging7Test/PagingTests.cs
+++ CodingDojo/Paging7Test/PagingTests.cs
@@ -5,7 +5,23 @@
 // -----------------------------------------------------------------------
 namespace Paging7Test
 {
+    using NUnit.Framework;
+    using Paging7;
+
+    [TestFixture]
     public class PagingTests
     {
+        [Test]
+        public void GetPaging_NoPages_ResultsEmptyPaging()
+        {
+            var target = new Paging();
+            var pages = 0;
+            var currentPage = 0;
+            var expected = string.Empty;
+
+            string actual = target.GetPaging(pages, currentPage);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
     }
 }
