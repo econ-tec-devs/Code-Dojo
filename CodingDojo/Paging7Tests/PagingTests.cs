d599e730f244cb02b8ee404d84a12529ce95689a
Dominik Hirsch <dhirsch@econ-tec.de>
2021-05-06 10:53:42 +0200

    initial Project

---
+++ CodingDojo/Paging7Tests/PagingTests.cs
@@ -0,0 +1,23 @@
+// -----------------------------------------------------------------------
+// <copyright file="PagingTests.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Paging7Tests
+{
+    using NUnit.Framework;
+
+    public class PagingTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
