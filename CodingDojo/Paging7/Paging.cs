99aa1d3a43ce68d8d79901a8d4435b637cf21ef5
Marcel Hinze <MHinze@econ-tec.de>
2021-05-20 11:20:23 +0200

    red Test

--- CodingDojo/Paging7/Paging.cs
+++ CodingDojo/Paging7/Paging.cs
@@ -9,7 +9,7 @@ namespace Paging7
     {
         public string GetPaging(int pageCount, int currentPage)
         {
-            var paging = string.Empty;
+            string paging = string.Empty;
             for (int i = 1; i <= pageCount; i++)
             {
                 if (i == 1)
