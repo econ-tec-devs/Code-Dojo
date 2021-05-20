7442d3e0563e96b478f2e6844b0cf40bbdbe7879
Dominik Hirsch <dhirsch@econ-tec.de>
2021-05-20 11:09:40 +0200

    first test green, refactored, red test for 1 page

--- CodingDojo/Paging7/Paging.cs
+++ CodingDojo/Paging7/Paging.cs
@@ -1,6 +1,15 @@
+// -----------------------------------------------------------------------
+// <copyright file="Paging.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
 namespace Paging7
 {
     public class Paging
     {
+        public string GetPages(int maxPages)
+        {
+            return string.Empty;
+        }
     }
 }
---
+++ CodingDojo/Paging7Test/Paging7Tests.cs
@@ -0,0 +1,46 @@
+// -----------------------------------------------------------------------
+// <copyright file="Paging7Tests.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Paging7Test
+{
+    using NUnit.Framework;
+    using Paging7;
+
+    public class Paging7Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Ctor_NewInstanceNoError()
+        {
+            var paging = new Paging();
+        }
+
+        [Test]
+        public void GetPages_MaxPages0_ReturnsEmptyString()
+        {
+            var target = new Paging();
+            var expected = string.Empty;
+
+            var actual = target.GetPages(0);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void GetPages_MaxPages1_ReturnsStringWith1()
+        {
+            var target = new Paging();
+            var expected = "1";
+
+            var actual = target.GetPages(1);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
