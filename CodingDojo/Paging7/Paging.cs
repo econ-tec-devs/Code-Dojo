3463112e1f1b7243cb4f6827abba4fd070914ec9
Rouven Lutz <rlutz@econ-tec.de>
2021-05-20 11:07:15 +0200

    Green Test: two pages

--- CodingDojo/Paging7/Paging.cs
+++ CodingDojo/Paging7/Paging.cs
@@ -1,16 +1,25 @@
+// -----------------------------------------------------------------------
+// <copyright file="Paging.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
 namespace Paging7
 {
     public class Paging
     {
         public string GetPaging(int pageCount, int currentPage)
         {
-            if (currentPage==1)
+            if (pageCount == 2)
+            {
+                return "(1) 2";
+            }
+
+            if (currentPage == 1)
             {
                 return "(1)";
             }
 
             return string.Empty;
-
         }
     }
 }
