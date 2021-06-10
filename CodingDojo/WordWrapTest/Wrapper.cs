921f3d1d5f104107a6fb45710d4c866eb24781a5
Marcel Hinze <MHinze@econ-tec.de>
2021-06-10 11:10:34 +0200

    Test Green

---
+++ CodingDojo/WordWrapTest/Wrapper.cs
@@ -0,0 +1,10 @@
+namespace WordWrapTest
+{
+    public class Wrapper
+    {
+        public static string Wrap(string rawString, int columnNumber)
+        {
+            return string.Empty;
+        }
+    }
+}
