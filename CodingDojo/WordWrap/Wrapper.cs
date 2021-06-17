a15042f579606201ad15c4aea8fa92b28fbe3b5d
Christian Lichter <clichter@econ-tec.de>
2021-06-17 11:02:02 +0200

    freen test

---
+++ CodingDojo/WordWrap/Wrapper.cs
@@ -0,0 +1,15 @@
+// -----------------------------------------------------------------------
+// <copyright file="Wrapper.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+namespace WordWrap
+{
+    public static class Wrapper
+    {
+        public static string Wrap(string text, int columnNumber)
+        {
+            return text;
+        }
+    }
+}
--- CodingDojo/WordWrapTest/WordWrapTests.cs
+++ CodingDojo/WordWrapTest/WordWrapTests.cs
@@ -6,6 +6,7 @@
 namespace WordWrapTest
 {
     using NUnit.Framework;
+    using WordWrap;
 
     public class WordWrapTests
     {
