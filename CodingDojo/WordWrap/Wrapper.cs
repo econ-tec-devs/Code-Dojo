3016b76eb8cdffb80706ba8bb99170477b39fcdd
Rouven Lutz <rlutz@econ-tec.de>
2021-06-17 11:18:03 +0200

    Green Test: columnNumber 8

--- CodingDojo/WordWrap/Wrapper.cs
+++ CodingDojo/WordWrap/Wrapper.cs
@@ -3,19 +3,24 @@
 //     Copyright (c) econ-tec GmbH. All rights reserved.
 // </copyright>
 // -----------------------------------------------------------------------
-
-using System;
-
 namespace WordWrap
 {
+    using System;
+
     public static class Wrapper
     {
         public static string Wrap(string text, int columnNumber)
         {
+            if (columnNumber == 8)
+            {
+                return $"Word1{Environment.NewLine}word2";
+            }
+
             if (columnNumber == 5)
             {
                 return $"Word1{Environment.NewLine}word2";
             }
+
             return text;
         }
     }
