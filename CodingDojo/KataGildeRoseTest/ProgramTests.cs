402487643338cad197ef73376f9101bed928c4a6
Rouven Lutz <rlutz@econ-tec.de>
2021-06-24 11:19:14 +0200

    Implemented Tests Project

---
+++ CodingDojo/KataGildeRoseTest/ProgramTests.cs
@@ -0,0 +1,27 @@
+// -----------------------------------------------------------------------
+// <copyright file="ProgramTests.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+namespace KataGildeRoseTest
+{
+    using KataGildedRose;
+    using NUnit.Framework;
+
+    public class ProgramTests
+    {
+        private Program _target;
+
+        [SetUp]
+        public void Setup()
+        {
+            _target = new Program();
+        }
+
+        [Test]
+        public void PublicItems_DoesExist_AssertsTrue()
+        {
+            Assert.That(_target.PublicItems, Is.Null);
+        }
+    }
+}
--- CodingDojo/KataGildedRose/Program.cs
+++ CodingDojo/KataGildedRose/Program.cs
@@ -1,37 +1,21 @@
-﻿using System.Collections.Generic;
-
+﻿// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
 namespace KataGildedRose
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
     {
-        IList<Item> Items;
-        static void Main(string[] args)
+        private IList<Item> Items;
+
+        public IList<Item> PublicItems
         {
-            System.Console.WriteLine("OMGHAI!");
-
-            var app = new Program()
-            {
-                Items = new List<Item>
-                                          {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          }
-
-            };
-
-            app.UpdateQuality();
-
-            System.Console.ReadKey();
-
+            get => Items;
+            set => Items = value;
         }
 
         public void UpdateQuality()
@@ -110,6 +94,32 @@ namespace KataGildedRose
             }
         }
 
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("OMGHAI!");
+
+            var app = new Program
+            {
+                Items = new List<Item>
+                {
+                    new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
+                    new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
+                    new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
+                    new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
+                    new Item
+                    {
+                        Name = "Backstage passes to a TAFKAL80ETC concert",
+                        SellIn = 15,
+                        Quality = 20
+                    },
+                    new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
+                }
+            };
+
+            app.UpdateQuality();
+
+            Console.ReadKey();
+        }
     }
 
     public class Item
