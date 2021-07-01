67e97753ff8ca66663a1c73f0373825330a41ed3
Christian Lichter <clichter@econ-tec.de>
2021-07-01 11:14:22 +0200

    SellIn 0

--- CodingDojo/KataGildeRoseTest/ProgramTests.cs
+++ CodingDojo/KataGildeRoseTest/ProgramTests.cs
@@ -67,7 +67,7 @@ namespace KataGildeRoseTest
         public void UpdateQuality_SetTwoNormalItems_NoSideEffects()
         {
             var firstItem = new Item { Name = "Kleines Ei", SellIn = 1, Quality = 25 };
-            var  secondItem= new Item { Name = "Schreiterfleisch", SellIn = 4, Quality = 30 };
+            var secondItem = new Item { Name = "Schreiterfleisch", SellIn = 4, Quality = 30 };
 
             _target.PublicItems = new List<Item> { firstItem, secondItem };
             _target.UpdateQuality();
@@ -76,6 +76,18 @@ namespace KataGildeRoseTest
             Assert.That(firstItem.Quality, Is.EqualTo(24));
             Assert.That(secondItem.SellIn, Is.EqualTo(3));
             Assert.That(secondItem.Quality, Is.EqualTo(29));
+        }
+
+        [Test]
+        public void UpdateQuality_SetOneItemSellIn0_ValuesDecreaseByTwo()
+        {
+            var firstItem = new Item { Name = "Kleines Ei", SellIn = 0, Quality = 25 };
+
+            _target.PublicItems = new List<Item> { firstItem };
+            _target.UpdateQuality();
+
+            Assert.That(firstItem.SellIn, Is.EqualTo(-1));
+            Assert.That(firstItem.Quality, Is.EqualTo(23));
         }
     }
 }
