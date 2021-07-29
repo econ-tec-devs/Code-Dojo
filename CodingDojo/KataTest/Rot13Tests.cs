78cd47ebb277e71a904b9c7f46d8829b92fcdae4
Ralf Schoch <rschoch@econ-tec.de>
2021-07-29 10:19:10 +0200

    Kata Rot 13 Vorlage

---
+++ CodingDojo/KataTest/Rot13Tests.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+
+namespace KataTest
+{
+    public class Rot13Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void METHOD_SCENARIO_EXPECTEDRESULT()
+        {
+            // Arrange
+            
+            // Act
+
+            // Assert
+        }
+    }
+}
