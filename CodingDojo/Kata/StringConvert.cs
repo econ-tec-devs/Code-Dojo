8b4d5ed143850c945708751894515f759278b945
Christian Lichter <clichter@econ-tec.de>
2021-08-05 11:12:44 +0200

    green test

--- CodingDojo/Kata/StringConvert.cs
+++ CodingDojo/Kata/StringConvert.cs
@@ -1,13 +1,22 @@
-using System.Collections.Generic;
-
-
-
+// -----------------------------------------------------------------------
+// <copyright file="StringConvert.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
 namespace Kata
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     public class StringConvert
     {
-        public Dictionary<string,string> ToDictionary(string input)
+        public Dictionary<string, string> ToDictionary(string input)
         {
+            if (input.Any())
+            {
+                return new Dictionary<string, string> { { "a", "1" } };
+            }
+
             return new Dictionary<string, string>();
         }
     }
