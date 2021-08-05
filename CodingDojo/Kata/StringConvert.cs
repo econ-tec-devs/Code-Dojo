94cc15bf3c00681a56198711281196e7a20251bc
Matthias Veenhuis <mveenhuis@econ-tec.de>
2021-08-05 11:15:51 +0200

    commit red test

---
+++ CodingDojo/Kata/StringConvert.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+
+
+namespace Kata
+{
+    public class StringConvert
+    {
+        public Dictionary<string,string> ToDictionary(string input)
+        {
+            return new Dictionary<string, string>();
+        }
+    }
+}
--- CodingDojo/KataTest/StringConvertTests.cs
+++ CodingDojo/KataTest/StringConvertTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Kata;
 
 namespace KataTest
 {
@@ -17,6 +18,21 @@ namespace KataTest
         {
             var input = string.Empty;
             var expected = new Dictionary<string, string>();
+            var stringConvert = new StringConvert();
+
+            var actual = stringConvert.ToDictionary(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+        
+        [Test]
+        public void ToDictionary_OneInput_DictionaryWithOneEntry()
+        {
+            var input = "a=1";
+            var expected = new Dictionary<string, string>
+            {
+                {"a","1"}
+            };
             var stringConvert = new StringConvert();
 
             var actual = stringConvert.ToDictionary(input);
