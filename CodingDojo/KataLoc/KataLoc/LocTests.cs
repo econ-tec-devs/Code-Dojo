fa41f9a49168b900111d2566b901ab709b2a2b6e
Christian Lichter <clichter@econ-tec.de>
2021-08-12 11:50:25 +0200

    red test

--- CodingDojo/KataLoc/KataLoc/LocTests.cs
+++ CodingDojo/KataLoc/KataLoc/LocTests.cs
@@ -1,8 +1,14 @@
-using KataLOC;
-using NUnit.Framework;
-
+// -----------------------------------------------------------------------
+// <copyright file="LocTests.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
 namespace KataLocTest
 {
+    using System;
+    using KataLOC;
+    using NUnit.Framework;
+
     [TestFixture]
     public class LocTests
     {
@@ -14,26 +20,35 @@ namespace KataLocTest
         [Test]
         public void LineOfCode_EmptyCode_ReturnsZero()
         {
-
-            var expected = new LinesCount() { CommentsWhitespaceLineCount = 0, LineOfCodeCount = 0 };
-            LinesCount actual = Loc.LineOfCode(string.Empty);
+            var expected = new LinesCount { CommentsWhitespaceLineCount = 0, LineOfCodeCount = 0 };
+            var actual = Loc.LineOfCode(string.Empty);
 
             Assert.That(actual.CommentsWhitespaceLineCount, Is.EqualTo(expected.CommentsWhitespaceLineCount));
             Assert.That(actual.LineOfCodeCount, Is.EqualTo(expected.LineOfCodeCount));
         }
-        
+
         [Test]
         public void LineOfCode_OneLineOfCode_ReturnOneLineCount()
         {
-            var text = $"Console.WriteLine()";
-            var expected = new LinesCount() { CommentsWhitespaceLineCount = 0, LineOfCodeCount = 1 };
+            var text = "Console.WriteLine()";
+            var expected = new LinesCount { CommentsWhitespaceLineCount = 0, LineOfCodeCount = 1 };
 
-            LinesCount actual = Loc.LineOfCode(text);
+            var actual = Loc.LineOfCode(text);
+
+            Assert.That(actual.CommentsWhitespaceLineCount, Is.EqualTo(expected.CommentsWhitespaceLineCount));
+            Assert.That(actual.LineOfCodeCount, Is.EqualTo(expected.LineOfCodeCount));
+        }
+
+        [Test]
+        public void LineOfCode_TwoLineOfCode_ReturnTwoLineCount()
+        {
+            var text = "Console.WriteLine()" + Environment.NewLine + "Bla";
+            var expected = new LinesCount { CommentsWhitespaceLineCount = 0, LineOfCodeCount = 2 };
+
+            var actual = Loc.LineOfCode(text);
 
             Assert.That(actual.CommentsWhitespaceLineCount, Is.EqualTo(expected.CommentsWhitespaceLineCount));
             Assert.That(actual.LineOfCodeCount, Is.EqualTo(expected.LineOfCodeCount));
         }
     }
-
-   
 }
