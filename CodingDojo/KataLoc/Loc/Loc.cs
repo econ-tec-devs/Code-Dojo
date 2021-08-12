f7745154b69d2de0b69b4922df2f167b92669fd6
Christian Lichter <clichter@econ-tec.de>
2021-08-12 11:47:46 +0200

    green test refactror

--- CodingDojo/KataLoc/Loc/Loc.cs
+++ CodingDojo/KataLoc/Loc/Loc.cs
@@ -1,10 +1,22 @@
+// -----------------------------------------------------------------------
+// <copyright file="Loc.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
 namespace KataLOC
 {
+    using System.Linq;
+
     public class Loc
     {
-        public static LinesCount LineOfCode(string empty)
+        public static LinesCount LineOfCode(string code)
         {
-            return new LinesCount() {LineOfCodeCount = 0, CommentsWhitespaceLineCount = 0};
+            if (code.Any())
+            {
+                return new LinesCount { LineOfCodeCount = 1, CommentsWhitespaceLineCount = 0 };
+            }
+
+            return new LinesCount { LineOfCodeCount = 0, CommentsWhitespaceLineCount = 0 };
         }
     }
 }
