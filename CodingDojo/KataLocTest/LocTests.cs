c3326dbad4c1cb9ea8aac075ad505c95e99ec0b0
Rouven Lutz <rlutz@econ-tec.de>
2021-08-19 08:20:46 +0200

    Init Test Project

---
+++ CodingDojo/KataLocTest/LocTests.cs
@@ -0,0 +1,23 @@
+// -----------------------------------------------------------------------
+// <copyright file="LocTests.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+namespace KataLocTest
+{
+    using NUnit.Framework;
+
+    public class LocTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
