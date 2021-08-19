34a3eb7424ec7cf0e4755051b770898160c540a6
Rouven Lutz <rlutz@econ-tec.de>
2021-08-19 08:45:24 +0200

    Refactored: implemented TestCase

--- CodingDojo/KataLocTest/CodeUtilsTests.cs
+++ CodingDojo/KataLocTest/CodeUtilsTests.cs
@@ -18,55 +18,12 @@ namespace KataLocTest
             _target = new CodeUtils();
         }
 
-        [Test]
-        public void Loc_InputEmptyString_TestPasses()
+        [TestCase(null, 0)]
+        [TestCase("", 0)]
+        [TestCase("line1", 1)]
+        [TestCase("line1\nline2", 2)]
+        public void Loc_InputString_ReturnsExpected(string input, int expected)
         {
-            var input = string.Empty;
-
-            _target.Loc(input);
-
-            Assert.Pass();
-        }
-
-        [Test]
-        public void Loc_InputEmptyString_ReturnsZero()
-        {
-            var input = string.Empty;
-            var expected = 0;
-
-            var actual = _target.Loc(input);
-
-            Assert.That(actual, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void Loc_InputNull_ReturnsZero()
-        {
-            string input = null;
-            var expected = 0;
-
-            var actual = _target.Loc(input);
-
-            Assert.That(actual, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void Loc_InputOneLine_ReturnsOne()
-        {
-            var input = "line";
-            var expected = 1;
-
-            var actual = _target.Loc(input);
-
-            Assert.That(actual, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void Loc_InputTwoLines_ReturnsTwo()
-        {
-            var input = "line1\nline2";
-            var expected = 2;
-
             var actual = _target.Loc(input);
 
             Assert.That(actual, Is.EqualTo(expected));
