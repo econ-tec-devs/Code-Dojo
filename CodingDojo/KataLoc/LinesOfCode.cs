b591d6e5e798d2a6b0bec888b4cc1bfcb69361ee
Rouven Lutz <rlutz@econ-tec.de>
2021-08-26 13:59:31 +0200

    Green Test: two lines of code

--- CodingDojo/KataLoc/LinesOfCode.cs
+++ CodingDojo/KataLoc/LinesOfCode.cs
@@ -1,5 +1,7 @@
 ﻿namespace KataLoc
 {
+    using System;
+
     public class LinesOfCode
     {
         public int Count(string input)
@@ -9,7 +11,9 @@
                 return 0;
             }
 
-            return 1;
+            var separator = new []{ Environment.NewLine};
+            var lines = input.Split(separator, StringSplitOptions.None);
+            return lines.Length;
         }
 
     }
