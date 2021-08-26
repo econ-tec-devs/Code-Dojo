35f92bd418f68d5227c438db66063bebb0779bd5
Marcel Hinze <MHinze@econ-tec.de>
2021-08-26 11:06:23 +0200

    Green Test

--- CodingDojo/Loc/LineCounter.cs
+++ CodingDojo/Loc/LineCounter.cs
@@ -3,12 +3,19 @@
 //     Copyright (c) econ-tec GmbH. All rights reserved.
 // </copyright>
 // -----------------------------------------------------------------------
+
+using System;
+
 namespace KataLoc
 {
     public class LineCounter
     {
         public int CountCodeLines(string lines)
         {
+            if (lines != string.Empty)
+            {
+                return 1;
+            }
             return 0;
         }
     }
