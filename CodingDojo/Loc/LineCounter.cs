16579305afd00b23d7ed1cade19a75415e96cfb5
Christian Lichter <clichter@econ-tec.de>
2021-08-26 11:25:46 +0200

    green test

--- CodingDojo/Loc/LineCounter.cs
+++ CodingDojo/Loc/LineCounter.cs
@@ -3,16 +3,19 @@
 //     Copyright (c) econ-tec GmbH. All rights reserved.
 // </copyright>
 // -----------------------------------------------------------------------
-
-using System;
-using System.Linq;
-
 namespace KataLoc
 {
+    using System.Linq;
+
     public class LineCounter
     {
         public int CountCodeLines(string lines)
         {
+            if (lines.StartsWith("/*"))
+            {
+                return 0;
+            }
+
             if (lines.StartsWith("//"))
             {
                 return 0;
