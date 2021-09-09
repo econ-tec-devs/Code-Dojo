7747c88e48218a7cea1010577ac09e6a18449264
Christian Lichter <clichter@econ-tec.de>
2021-09-09 11:09:48 +0200

    green test

--- CodingDojo/KataLOC/LineCounter.cs
+++ CodingDojo/KataLOC/LineCounter.cs
@@ -1,20 +1,24 @@
-﻿using System;
-
+﻿// -----------------------------------------------------------------------
+// <copyright file="LineCounter.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
 namespace KataLOC
 {
+    using System;
+
     public class LineCounter
     {
-        public LineCounter()
-        {
-        }
-
         public int Count(string empty)
         {
             if (string.IsNullOrEmpty(empty))
             {
                 return 0;
             }
-            return 1;
+
+            var lines = empty.Split(Environment.NewLine);
+
+            return lines.Length;
         }
     }
 }
