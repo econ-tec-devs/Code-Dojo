e5c2ab7e60a9958acfad90a333bd9b3a100eb18d
Christian Lichter <clichter@econ-tec.de>
2021-09-09 11:13:27 +0200

    red test

--- CodingDojo/KataLocTest/LocTests.cs
+++ CodingDojo/KataLocTest/LocTests.cs
@@ -1,8 +1,12 @@
-using System;
-using KataLOC;
-
+// -----------------------------------------------------------------------
+// <copyright file="LocTests.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
 namespace KataLOCTest
 {
+    using System;
+    using KataLOC;
     using NUnit.Framework;
 
     public class LocTests
@@ -19,8 +23,8 @@ namespace KataLOCTest
         public void Count_EmptyString_0()
         {
             var expected = 0;
-            
-            int actual = _target.Count(string.Empty);
+
+            var actual = _target.Count(string.Empty);
 
             Assert.That(actual, Is.EqualTo(expected));
         }
@@ -31,18 +35,29 @@ namespace KataLOCTest
             var expected = 1;
             var text = "line1";
 
-            int actual = _target.Count(text);
+            var actual = _target.Count(text);
 
             Assert.That(actual, Is.EqualTo(expected));
         }
-        
+
         [Test]
         public void Count_TwoLineOfCode_2()
         {
             var expected = 2;
             var text = "line1" + Environment.NewLine + "line2";
 
-            int actual = _target.Count(text);
+            var actual = _target.Count(text);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Count_ThreeLinesOneEmptyLine_2()
+        {
+            var expected = 2;
+            var text = "line1" + Environment.NewLine + "line2" + Environment.NewLine + "";
+
+            var actual = _target.Count(text);
 
             Assert.That(actual, Is.EqualTo(expected));
         }
