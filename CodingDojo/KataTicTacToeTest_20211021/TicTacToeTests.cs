2ec22fd003f8ab99096650c741480088ad10e542
Corinne Tzebia <ctzebia@econ-tec.de>
2021-11-19 11:11:53 +0100

    TestCase C0 of Move_SecondMoveInputValidCoordinate_ReturnBoardWithOneXAndOneO made red

--- CodingDojo/KataTicTacToeTest_20211021/TicTacToeTests.cs
+++ CodingDojo/KataTicTacToeTest_20211021/TicTacToeTests.cs
@@ -25,9 +25,26 @@ namespace KataTicTacToeTest_20211021
         [TestCase("B0", " X       ")]
         [TestCase("C0", "  X      ")] 
         [TestCase("A1", "   X     ")]
+        [TestCase("B1", "    X    ")]
+        [TestCase("C1", "     X   ")]
+        [TestCase("A2", "      X  ")]
+        [TestCase("B2", "       X ")]
+        [TestCase("C2", "        X")]
         public void Move_FirstMoveInputValidCoordinate_ReturnBoardWithOneX(string coordinate, string expectedBoard)
         {
             var target = new TicTacToe();
+            
+            string actual = target.Move(coordinate);
+
+            Assert.That(actual, Is.EqualTo(expectedBoard));
+        }
+        
+        [TestCase("C0", "X O      ")]
+        public void Move_SecondMoveInputValidCoordinate_ReturnBoardWithOneXAndOneO(string coordinate, string expectedBoard)
+        {
+            var target = new TicTacToe();
+            target.Initialize();
+            target.Move("A0");
             
             string actual = target.Move(coordinate);
 
