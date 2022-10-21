3052dac22017ca5e447c8f3033b1d5d9b729844c
Nandor Schaffhauser <nschaffhauser@econ-tec.de>
2022-10-21 14:46:58 +0200

    Irgendwas hinzugefügt

---
+++ CodingDojo/KataFizzBuzz/KataFizzBuzz/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace KataFizzBuzz
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Irgendwas");
+        }
+    }
+}
