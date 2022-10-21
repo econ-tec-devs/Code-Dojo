1c749945de22542947aac0c169cf4abd40f3f0ef
Rouven Lutz <rlutz@econ-tec.de>
2022-10-21 14:54:23 +0200

    Noch ne Zeile

--- CodingDojo/KataFizzBuzz/KataFizzBuzz/Program.cs
+++ CodingDojo/KataFizzBuzz/KataFizzBuzz/Program.cs
@@ -9,7 +9,7 @@
             Console.WriteLine("Hello World!");
             Console.WriteLine("Irgendwas");
             Console.WriteLine("Seit faul");
-            
+            Console.WriteLine("Seit faul");            
         }
     }
 }
