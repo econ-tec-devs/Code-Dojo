9138f5dc27bbd84eac75a3dfc08915c3e749558f
Rouven Lutz <rlutz@econ-tec.de>
2022-10-21 14:31:14 +0200

    Init repo with new c# console app

---
+++ CodingDojo/KataFizzBuzz/KataFizzBuzz/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace KataFizzBuzz
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
