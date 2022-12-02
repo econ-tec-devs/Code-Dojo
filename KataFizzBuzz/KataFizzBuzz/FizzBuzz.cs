f213fe42acbf7ec20e999568b24df51f180b0a61
Rouven Lutz <rlutz@econ-tec.de>
2022-12-02 14:25:38 +0100

    Red Test: return 1

---
+++ KataFizzBuzz/KataFizzBuzz/FizzBuzz.cs
@@ -0,0 +1,14 @@
+// -----------------------------------------------------------------------
+// <copyright file="FizzBuzz.cs" company="econ-tec GmbH">
+//     Copyright (c) econ-tec GmbH. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+namespace KataFizzBuzz
+{
+    public class FizzBuzz
+    {
+        public void Print()
+        {
+        }
+    }
+}
---
+++ KataFizzBuzz/KataFizzBuzzTest/FizzBuzzTests.cs
@@ -0,0 +1,46 @@
+using NUnit.Framework;
+
+namespace KataFizzBuzzTest
+{
+    using System;
+    using System.IO;
+    using KataFizzBuzz;
+
+    public class FizzBuzzTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Print_Called_TestPassed()
+        {
+            // Arrange
+            var sut = new FizzBuzz();
+            
+            // Act
+            sut.Print();
+            
+            // Assert
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Print_Called_Returns1()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            var stringBulder = writer.GetStringBuilder();
+            var expected = "1";
+            var sut = new FizzBuzz();
+            
+            // Act
+            sut.Print();
+            
+            // Assert
+            Assert.That(stringBulder.ToString(), Is.EqualTo(expected));
+        }
+    }
+}
