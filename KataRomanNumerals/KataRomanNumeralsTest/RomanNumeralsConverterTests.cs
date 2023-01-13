7ff754eb72bc9ea1706b6eb9ea08bddf909e60ab
Rouven Lutz <rlutz@econ-tec.de>
2023-01-13 14:33:46 +0100

    Init Solution with red test: ctor called

---
+++ KataRomanNumerals/KataRomanNumeralsTest/RomanNumeralsConverterTests.cs
@@ -0,0 +1,19 @@
+namespace KataRomanNumeralsTest;
+
+public class RomanNumeralsConverterTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Ctor_Called_Passes()
+    {
+        // Arrange & Act
+        var sut = new RomanNumeralsConverter();
+        
+        // Assert
+        Assert.Pass();
+    }
+}
