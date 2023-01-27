b4f38060eeedfc763bc116d42d875c09684a1fd3
Zoubida Laiche <zlaiche@econ-tec.de>
2023-01-27 15:13:18 +0100

    Refactor

--- Kata/RomanNumeralsTest/RomanNumeralsConverterTests.cs
+++ Kata/RomanNumeralsTest/RomanNumeralsConverterTests.cs
@@ -9,7 +9,7 @@ public class RomanNumeralsConverterTests
     [TestCase(3, "III")]
     [TestCase(4, "IV")]
     [TestCase(5, "V")]
-    public void ToRoman_Input3_ReturnsIII(int input, string expected)
+    public void ToRoman_InputNumbers_ReturnsRomanNumerals(int input, string expected)
     {
         // Arrange
         var sut = new RomanNumeralsConverter();
