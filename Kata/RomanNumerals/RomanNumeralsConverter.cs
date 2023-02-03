57b8bd94849895a57a0cdaf20ec89d47a8f4783e
Rouven Lutz <rlutz@econ-tec.de>
2023-02-03 15:00:17 +0100

    Refactored: returns X

--- Kata/RomanNumerals/RomanNumeralsConverter.cs
+++ Kata/RomanNumerals/RomanNumeralsConverter.cs
@@ -5,34 +5,9 @@ public class RomanNumeralsConverter
     public string ToRoman(int number)
     {
         var output = string.Empty;
-        if (number == 10)
-        {
-            return "X";
-        }
-        
         if (number == 9)
         {
             return "IX";
-        }
-        
-        if (number == 8)
-        {
-            return "VIII";
-        }
-        
-        if (number == 7)
-        {
-            return "VII";
-        }
-        
-        if (number == 6)
-        {
-            return "VI";
-        }
-        
-        if (number == 5)
-        {
-            return "V";
         }
         
         if (number == 4)
@@ -40,11 +15,27 @@ public class RomanNumeralsConverter
             return "IV";
         }
 
+        output += Number(ref number, 10, "X");
+        output += Number(ref number, 5, "V");
+
         for (var i = 1; i <= number; i++)
         {
             output += "I";
         }
         
+        return output;
+    }
+
+    private string Number(ref int number, int divisor, string romanNumeral)
+    {
+        var output = string.Empty;
+        var modulo = number % divisor;
+        if (modulo >= 0 && modulo != number)
+        {
+            output += romanNumeral;
+            number -= divisor;
+        }
+
         return output;
     }
 }
