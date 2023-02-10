a2595559a96199adf15a2c3d7c5db0fc0cfec56c
Rouven Lutz <rlutz@econ-tec.de>
2023-02-10 11:10:50 +0100

    Finito

--- Kata/RomanNumerals/RomanNumeralsConverter.cs
+++ Kata/RomanNumerals/RomanNumeralsConverter.cs
@@ -5,6 +5,14 @@ public class RomanNumeralsConverter
     public string ToRoman(int number)
     {
         var output = string.Empty;
+        output += Number(ref number, 1000, "M");
+        output += Number(ref number, 900, "CM");
+        output += Number(ref number, 500, "D");
+        output += Number(ref number, 400, "CD");
+        output += Number(ref number, 100, "C");
+        output += Number(ref number, 90, "XC");
+        output += Number(ref number, 50, "L");
+        output += Number(ref number, 40, "XL");
         output += Number(ref number, 10, "X");
         output += Number(ref number, 9, "IX");
         output += Number(ref number, 5, "V");
