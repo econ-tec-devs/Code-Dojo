71ca1ef5cac9621a21599ca16313943a2bf8fa5d
Zoubida Laiche <zlaiche@econ-tec.de>
2023-02-17 15:15:18 +0100

    Red Test:return 8

--- Kata/KataPotterTest/ShoppingCartTests.cs
+++ Kata/KataPotterTest/ShoppingCartTests.cs
@@ -2,17 +2,22 @@ namespace KataPotterTest;
 
 public class ShoppingCartTests
 {
+    private ShoppingCart sut;
+
     [SetUp]
     public void Setup()
-    {
+    { 
+        sut = new ShoppingCart(); 
+        
     }
 
     [Test]
     public void Checkout_Called_Passed()
     {
         //Arrange
-        var sut = new ShoppingCart(); 
+        
         //Act
+        
         sut.Checkout(null);
         //Assert
         Assert.Pass();
@@ -22,23 +27,38 @@ public class ShoppingCartTests
     public void Checkout_InputStringList_Passed()
     {
         //Arrange
-        var sut = new ShoppingCart();
+     
         var books = new List<string>();
         //Act
         sut.Checkout(books);
         //Assert
         Assert.Pass();
         
-    }[Test]
+    }
+    
+    [Test]
     public void Checkout_InputEmptyStringList_ReturnPriceDecimal()
     {
         //Arrange
-        var sut = new ShoppingCart();
+        
         var books = new List<string>();
         //Act
         decimal price = sut.Checkout(books);
         //Assert
         Assert.That(price, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void Checkout_InputOneBook_Return8()
+    {
+        //Arrange
+        var expected = 8;
+        var books = new List<string> {"Book1"};
+        //Act
+        decimal price = sut.Checkout(books);
+        
+        //Assert
+        Assert.That(price, Is.EqualTo(expected));
     }
 }
 
