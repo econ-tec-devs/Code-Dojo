7e49cf4c48b843e6dd7987c0ab23bfcc3f456427
Dominik Hirsch <dhirsch@econ-tec.de>
2023-02-17 15:22:35 +0100

    fixed test one book; refactored; new red test two equal books;

--- Kata/KataPotter/ShoppingCart.cs
+++ Kata/KataPotter/ShoppingCart.cs
@@ -5,6 +5,13 @@ public class ShoppingCart
     
     public decimal Checkout(List<string> books)
     {
+        if (books == null) return 0;
+        
+        if (books.Any())
+        {
+            return 8;
+        }
+        
         return 0;
     }
 }
--- Kata/KataPotterTest/ShoppingCartTests.cs
+++ Kata/KataPotterTest/ShoppingCartTests.cs
@@ -7,18 +7,15 @@ public class ShoppingCartTests
     [SetUp]
     public void Setup()
     { 
-        sut = new ShoppingCart(); 
-        
+        sut = new ShoppingCart();
     }
 
     [Test]
     public void Checkout_Called_Passed()
     {
-        //Arrange
-        
         //Act
-        
         sut.Checkout(null);
+        
         //Assert
         Assert.Pass();
     }
@@ -27,10 +24,11 @@ public class ShoppingCartTests
     public void Checkout_InputStringList_Passed()
     {
         //Arrange
-     
         var books = new List<string>();
+        
         //Act
         sut.Checkout(books);
+        
         //Assert
         Assert.Pass();
         
@@ -40,12 +38,13 @@ public class ShoppingCartTests
     public void Checkout_InputEmptyStringList_ReturnPriceDecimal()
     {
         //Arrange
-        
         var books = new List<string>();
+        
         //Act
-        decimal price = sut.Checkout(books);
+        var actual = sut.Checkout(books);
+        
         //Assert
-        Assert.That(price, Is.EqualTo(0));
+        Assert.That(actual, Is.EqualTo(0));
     }
     
     [Test]
@@ -54,11 +53,26 @@ public class ShoppingCartTests
         //Arrange
         var expected = 8;
         var books = new List<string> {"Book1"};
+        
         //Act
-        decimal price = sut.Checkout(books);
+        var actual = sut.Checkout(books);
         
         //Assert
-        Assert.That(price, Is.EqualTo(expected));
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void Checkout_InputTwoEqualBooks_Return16()
+    {
+        //Arrange
+        var expected = 16;
+        var books = new List<string> {"Book1", "Book1"};
+        
+        //Act
+        var actual = sut.Checkout(books);
+        
+        //Assert
+        Assert.That(actual, Is.EqualTo(expected));
     }
 }
 
