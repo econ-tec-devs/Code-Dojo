cb3bd448874245e2dfa249267afb6e2188d9121b
DarkShadow0711 <116357331+DarkShadow0711@users.noreply.github.com>
2023-03-03 14:21:18 +0100

    red test

---
+++ Kata/KataPotterTest/ShoppingCartTests.cs
@@ -0,0 +1,20 @@
+namespace KataPotterTest;
+
+public class ShoppingCartTests
+{
+    
+
+    [Test]
+    public void Checkout_Called_Passed()
+    {
+        //Arrange
+        var sut = new ShoppingCart();
+       
+        //Act
+        sut.Checkout();
+
+        //Assert
+        Assert.Pass();
+    }
+
+}
