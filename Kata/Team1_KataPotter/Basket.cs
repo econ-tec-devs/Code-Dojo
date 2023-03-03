00841db4c8c12117191ee40ba1bb4faa638dbe6d
sch.nandor@web.de <sch.nandor@web.de>
2023-03-03 14:36:44 +0100

    green test inputonebook

--- Kata/Team1_KataPotter/Basket.cs
+++ Kata/Team1_KataPotter/Basket.cs
@@ -3,7 +3,11 @@ namespace Team1_KataPotter;
 public class Basket
 {
     public decimal Checkout(List<string> books)
-    {   
+    {
+        if (books.Any())
+        {
+            return 8.00m;
+        }
         return 0;
     }
 }
