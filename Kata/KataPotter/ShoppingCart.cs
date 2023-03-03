ae6b2d14487058d97dd454d5a3cc0ceb1406720e
Zoubida Laiche <zlaiche@econ-tec.de>
2023-03-03 15:00:09 +0100

    Red Test: return 8

--- Kata/KataPotter/ShoppingCart.cs
+++ Kata/KataPotter/ShoppingCart.cs
@@ -3,8 +3,14 @@
 public class ShoppingCart
 {
     public decimal Checkout(List<string> books)
-    {
+    { 
+       
+        if (books.Any())
+        {
+            return 8;
+        }
         return 0;
+        
     }
    
 }
