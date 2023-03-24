1c18506535ebdd338e33be5297080f9ee133de7f
sch.nandor@web.de <sch.nandor@web.de>
2023-03-24 15:16:05 +0100

    work in progress ThreeBooksOneEqual

--- Kata/Team1_KataPotter/Basket.cs
+++ Kata/Team1_KataPotter/Basket.cs
@@ -7,13 +7,21 @@ public class Basket
         var price = 0m;
         decimal[] discount = { 0m, 1m, 0.95m, 0.9m, 0.8m, 0.75m };
         var count = 0;
+   
+        var distinctBooks = books.Distinct();
+
+        price = distinctBooks.Count() * 8 * discount[distinctBooks.Count()];
+
+        foreach (var book in distinctBooks)
+        {
+            books.Remove(book);
+        }
         
         foreach (var book in books)
         {
             price += 8.00m;
-            count++;
         }
 
-        return price * discount[books.Distinct().Count()];        
+        return price;
     }
 }
