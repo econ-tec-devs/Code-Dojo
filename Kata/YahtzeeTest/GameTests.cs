cdd9fe23209e49e8534853fbd5235cb7c0b66f83
rlutz@econ-tec.de <rlutz@econ-tec.de>
2023-05-12 14:56:11 +0200

    Refactor

--- Kata/YahtzeeTest/GameTests.cs
+++ Kata/YahtzeeTest/GameTests.cs
@@ -4,33 +4,24 @@ namespace YahtzeeTest;
 
 public class GameTests
 {
+    private Game _sut;
+
     [SetUp]
     public void Setup()
     {
-    }
-
-    [Test]
-    public void Ctor_Called_Pass()
-    {
-        // Arrange & Act
-        var sut = new Game();
-
-        // Assert
-        Assert.Pass();
+        _sut = new Game();
     }
 
     [Test]
     public void RollDice_Called_ReturnFiveNumbers()
     {
         // Arrange 
-        var sut = new Game();
-        int[] expected = new int[5];
+        var expected = new int[5];
 
         //Act
-        int[] actual = sut.RollDice();
-        
+        var actual = _sut.RollDice();
+
         // Assert
         Assert.That(actual, Is.EqualTo(expected));
-
     }
 }
