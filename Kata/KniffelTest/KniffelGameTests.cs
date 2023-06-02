1c8784b4fbc45d4e5a7d8dbaa85312fe40c0f95a
DarkShadow0711 <116357331+DarkShadow0711@users.noreply.github.com>
2023-06-02 14:55:48 +0200

    red Test

--- Kata/KniffelTest/KniffelGameTests.cs
+++ Kata/KniffelTest/KniffelGameTests.cs
@@ -34,5 +34,23 @@ public class KniffelGameTests
         // Assert
         Assert.That(actual, Is.EqualTo(expected));
     }
+    
+    [Test]
+    public void Dice_Called_NumbersBiggerThan0()
+    {
+        // Arrange
+        var sut = new KniffelGame();
+        int[] expected = new[] { 0, 0, 0, 0, 0 };
+        
+        // Act
+        int[] actual = sut.Dice();
+        
+        // Assert
+        foreach (var i in actual)
+        {
+           Assert.That(i, Is.GreaterThan(0)); 
+           
+        }
+    }
 }
 
