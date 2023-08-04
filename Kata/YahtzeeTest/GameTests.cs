afba91511b93cfc5617813c2c7a098aa49e10405
rlutz@econ-tec.de <rlutz@econ-tec.de>
2023-08-04 15:26:36 +0200

    Red Test: Two pairs but one number exists three times

--- Kata/YahtzeeTest/GameTests.cs
+++ Kata/YahtzeeTest/GameTests.cs
@@ -71,6 +71,7 @@ public class GameTests
     [TestCase(new [] {1, 2, 3, 4, 5}, Categories.Pair, 0)]
     [TestCase(new [] {1, 1, 2, 3, 3}, Categories.TwoPairs, 8)]
     [TestCase(new [] {4, 4, 2, 3, 3}, Categories.TwoPairs, 14)]
+    [TestCase(new [] {4, 4, 4, 3, 3}, Categories.TwoPairs, 14)]
     [TestCase(new [] {1, 2, 3, 4, 4}, Categories.TwoPairs, 0)]
     [TestCase(new [] {1, 2, 3, 4, 5}, Categories.TwoPairs, 0)]
     public void PlaceRoll_InputDiceAndCategory_ReturnPoints(int[] dice, Categories category, int expected)
