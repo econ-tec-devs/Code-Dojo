ae8f4265a4b4a9ce8588de20aa0a04b7523c49d8
rlutz@econ-tec.de <rlutz@econ-tec.de>
2023-09-04 11:13:24 +0200

    Examples for XMLHttpRequest, Promise and Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  private readonly url: string =
+    'https://petstore3.swagger.io/api/v3/pet/findByStatus?status=available';
+
+  constructor(private readonly httpClient: HttpClient) {}
+
+  public loadByXMLHttpRequest(): void {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = (): void => console.log(xhr.response);
+    xhr.open('GET', this.url, true);
+    xhr.send();
+
+    console.log('loadByXMLHttpRequest');
+  }
+
+  public loadByObservable(): void {
+    this.httpClient
+      .get(this.url)
+      .subscribe((response) => console.log(response));
+
+    console.log('loadByObservable');
+  }
+
+  public loadByPromise(): void {
+    this.httpClient
+      .get(this.url)
+      .toPromise()
+      .then((value) => console.log(value));
+
+    console.log('loadByPromise');
+  }
+}
