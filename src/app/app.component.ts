0718dd3ceed445183c05a6cb9d068f7a61e55b0d
rlutz@econ-tec.de <rlutz@econ-tec.de>
2023-09-05 12:02:56 +0200

    Refactored Test: place 51323 on ThreeOfAKind; returns 0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,54 +12,40 @@ export class AppComponent {
       return 0;
     }
 
-    let returnValue = 0;
-    const pairs: number[] = [];
+    let pairs: number[] = [];
 
     switch (category) {
       case Category.Ones:
-        return roll.filter((dice) => dice === 1).length;
       case Category.Twos:
-        return roll.filter((dice) => dice === 2).length * 2;
-
       case Category.Threes:
-        return roll.filter((dice) => dice === 3).length * 3;
-
       case Category.Fours:
-        return roll.filter((dice) => dice === 4).length * 4;
-
       case Category.Fives:
-        return roll.filter((dice) => dice === 5).length * 5;
-
       case Category.Sixes:
-        return roll.filter((dice) => dice === 6).length * 6;
+        return roll.filter((dice) => dice === category).length * category;
 
       case Category.Pair:
-        roll.sort().forEach((value, index) => {
-          roll?.forEach((value2, index2) => {
-            if (value === value2 && index !== index2) {
-              returnValue = value * 2;
-            }
-          });
-        });
-        return returnValue;
+        pairs = this.getPairs(roll);
+        return pairs.length >= 2 ? pairs[pairs.length - 1] : 0;
 
       case Category.TwoPairs:
-        roll.sort().forEach((value, index) => {
-          roll?.forEach((value2, index2) => {
-            if (value === value2 && index !== index2) {
-              pairs.push(value * 2);
-            }
-          });
-        });
-
-        if (pairs.length === 4) {
-          return pairs[0] + pairs[2];
-        }
-
-        return 0;
+        pairs = this.getPairs(roll);
+        return pairs.length === 4 ? pairs[0] + pairs[2] : 0;
 
       case Category.ThreeOfAKind:
         return 0;
     }
+  }
+
+  private getPairs(roll: number[]): number[] {
+    const pairs: number[] = [];
+    roll.sort().forEach((value, index, sorted) => {
+      sorted?.forEach((value2, index2) => {
+        if (value === value2 && index !== index2) {
+          pairs.push(value * 2);
+        }
+      });
+    });
+
+    return pairs;
   }
 }
