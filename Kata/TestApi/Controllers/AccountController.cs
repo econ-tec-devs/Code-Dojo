d2f4b5be0bf727961aa838c5312864919b711f8e
nschaffhauser <sch.nandor@web.de>
2023-11-21 11:54:48 +0100

    added new action email confirm

--- Kata/TestApi/Controllers/AccountController.cs
+++ Kata/TestApi/Controllers/AccountController.cs
@@ -13,4 +13,21 @@ public class AccountController: ControllerBase
     {
         return Ok(true);
     }
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public ActionResult<bool> Confirm(Account account)
+    {
+        if (string.IsNullOrWhiteSpace(account.Email))
+        {
+            return BadRequest(false);
+        }
+        return Ok(true);
+    }
+}
+
+public class Account
+{
+    public string Email { get; set; }
 }
--- Kata/TestApi/Program.cs
+++ Kata/TestApi/Program.cs
@@ -1,14 +1,18 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: "YourNeyCorsPolicy", policy  => policy.AllowAnyOrigin());
+    options.AddPolicy(name: "YourNeyCorsPolicy", policy  => policy.AllowAnyOrigin().AllowAnyMethod().WithHeaders("content-type"));
 });
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();   
 
 var app = builder.Build();
 
