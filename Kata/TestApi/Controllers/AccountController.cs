0f074f5942abf918fa1bc2895cd36a135bf73d3f
zlaiche <zlaiche@econ-tec.de>
2023-11-21 10:51:09 +0100

    Added AccountController

---
+++ Kata/TestApi/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestApi.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class AccountController: ControllerBase
+{
+    [HttpGet]
+    public ActionResult<bool> Register()
+    {
+        return Ok();
+    }
+}
