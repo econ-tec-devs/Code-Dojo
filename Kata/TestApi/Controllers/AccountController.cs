e20b1341e1ea4cd91e31342e779b6542ae0bf0c8
rlutz@econ-tec.de <rlutz@econ-tec.de>
2023-11-21 11:05:51 +0100

    Described action results and returned boolean

--- Kata/TestApi/Controllers/AccountController.cs
+++ Kata/TestApi/Controllers/AccountController.cs
@@ -6,8 +6,11 @@ namespace TestApi.Controllers;
 public class AccountController: ControllerBase
 {
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public ActionResult<bool> Register()
     {
-        return Ok();
+        return Ok(true);
     }
 }
