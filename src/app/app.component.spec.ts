adbf23f9b5c30396979a0d0a759a3614df7b082b
nschaffhauser <sch.nandor@web.de>
2024-01-30 10:51:17 +0100

    red test input 1 result I

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  test('ctor_called_returnsNewInstance', () => {
+  test('toRoman_input0_returnEmptyString', () => {
     // Arrange
     const sut = new AppComponent();
 
@@ -10,5 +10,16 @@ describe('AppComponent', () => {
 
     // Assert
     expect(actual).toBe('');
+  });
+
+  test('toRoman_input1_returnI', () => {
+    // Arrange
+    const sut = new AppComponent();
+
+    // Act
+    const actual: string = sut.toRoman(1);
+
+    // Assert
+    expect(actual).toBe('I');
   });
 });
