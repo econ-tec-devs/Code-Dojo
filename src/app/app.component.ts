544f1ed6fc94256145eef96b38d08b1b22683ada
nschaffhauser <sch.nandor@web.de>
2024-01-30 10:49:04 +0100

    green test input 0 result empty string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  public toRoman(number: number): string {
+    return "";
+  }
+}
