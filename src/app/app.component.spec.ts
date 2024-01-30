eb042c8b693a903671640a7940524b72d8ec96b6
rlutz@econ-tec.de <rlutz@econ-tec.de>
2024-01-30 10:46:23 +0100

    Red test: input 0, result empty string

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,13 @@ import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   test('ctor_called_returnsNewInstance', () => {
-    expect(new AppComponent()).toBeTruthy();
+    // Arrange
+    const sut = new AppComponent();
+
+    // Act
+    const actual: string = sut.toRoman(0);
+
+    // Assert
+    expect(actual).toBe('');
   });
 });
